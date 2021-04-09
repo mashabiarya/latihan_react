374e9969ac2cf48e823951cc1affdabac8489e20
mashabiarya <aryakasbi@gmail.com>
2021-04-09 19:33:26 +0700

    me

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Details from './pages/Details';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Redux } from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Redux>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/About' component={About} />
+            <Route path='/Blog' component={Blog} />
+            <Route path='/Details' component={Details} />
+            <Route path='/Contact' component={Contact} />
+          </Switch>
+        </Router>
+      </Redux>
+      <Footer />
     </div>
   );
 }
